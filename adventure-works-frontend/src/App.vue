5028d072d300af26086956531a35d717f1dca45b
piatek29535 <adamex12343@gmail.com>
2021-09-17 16:44:31 +0200

    init commit

---
+++ adventure-works-frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <VCategoryList/>
+</template>
+
+<script>
+import VCategoryList from './components/VCategoryList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VCategoryList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ adventure-works-frontend/src/components/VCategoryList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1 class="adventureListHeader">Adventure Works Category List</h1>
+  <ul id="categoryList">
+  </ul>
+  <button v-on:click="test">Click</button>
+  <p>Cześć {{msg}}</p>
+</template>
+
+<script>
+const VCategoryList = {
+  data(){
+    return{
+       
+    }
+  },
+  methods:{
+    test(){
+      const list = document.getElementById('categoryList')
+
+      const digits = [1,2,3,4,5,6,7,8].map(i =>{
+        const liElement = document.createElement('li');
+        liElement.innerText = i;
+        return liElement;
+      });
+
+      list.append(...digits)
+    }
+  }
+}
+export default VCategoryList;
+</script>
+
+<style scoped>
+  
+</style>
